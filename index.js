cb2003eb1e309310fcb41b99e282ebf1a93bfbf1
0xaplus <codewithaplus@gmail.com>
2024-12-09 20:05:08 +0100

    catch error

--- index.js
+++ index.js
@@ -31,69 +31,74 @@ const port = process.env.PORT;
 const payeePK = process.env.PAYEE_PK;
 
 app.post("/create-request", async (req, res) => {
-  const { amount, payerAddress } = req.body;
-
-  const epkSignatureProvider = new EthereumPrivateKeySignatureProvider({
-    method: Types.Signature.METHOD.ECDSA,
-    privateKey: payeePK,
-  });
-
-  const requestClient = new RequestNetwork({
-    nodeConnectionConfig: {
-      baseURL: "https://sepolia.gateway.request.network/",
-    },
-    signatureProvider: epkSignatureProvider,
-  });
-
-  const payeeIdentity = new Wallet(payeePK).address;
-  const payerIdentity = payerAddress;
-  const paymentRecipient = payeeIdentity;
-  const feeRecipient = utils.parseUnits("0", 18).toString();
-
-  const requestCreateParameters = {
-    requestInfo: {
-      currency: {
-        type: Types.RequestLogic.CURRENCY.ERC20,
-        value: "0x370DE27fdb7D1Ff1e1BaA7D11c5820a324Cf623C",
-        network: "sepolia",
+  try {
+    
+    const { amount, payerAddress } = req.body;
+  
+    const epkSignatureProvider = new EthereumPrivateKeySignatureProvider({
+      method: Types.Signature.METHOD.ECDSA,
+      privateKey: payeePK,
+    });
+  
+    const requestClient = new RequestNetwork({
+      nodeConnectionConfig: {
+        baseURL: "https://sepolia.gateway.request.network/",
       },
-      expectedAmount: utils.parseUnits(amount, 18).toString(),
-      payee: {
+      signatureProvider: epkSignatureProvider,
+    });
+  
+    const payeeIdentity = new Wallet(payeePK).address;
+    const payerIdentity = payerAddress;
+    const paymentRecipient = payeeIdentity;
+    const feeRecipient = utils.parseUnits("0", 18).toString();
+  
+    const requestCreateParameters = {
+      requestInfo: {
+        currency: {
+          type: Types.RequestLogic.CURRENCY.ERC20,
+          value: "0x370DE27fdb7D1Ff1e1BaA7D11c5820a324Cf623C",
+          network: "sepolia",
+        },
+        expectedAmount: utils.parseUnits(amount, 18).toString(),
+        payee: {
+          type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+          value: payeeIdentity,
+        },
+        payer: {
+          type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
+          value: payerIdentity,
+        },
+        timestamp: Utils.getCurrentTimestampInSecond(),
+      },
+      paymentNetwork: {
+        id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
+        parameters: {
+          paymentNetworkName: "sepolia",
+          paymentAddress: paymentRecipient,
+          feeAddress: feeRecipient,
+          feeAmount: "0",
+        },
+      },
+      contentData: {
+        reason: "ð",
+        dueDate: "",
+      },
+      signer: {
         type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
         value: payeeIdentity,
       },
-      payer: {
-        type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-        value: payerIdentity,
-      },
-      timestamp: Utils.getCurrentTimestampInSecond(),
-    },
-    paymentNetwork: {
-      id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
-      parameters: {
-        paymentNetworkName: "sepolia",
-        paymentAddress: paymentRecipient,
-        feeAddress: feeRecipient,
-        feeAmount: "0",
-      },
-    },
-    contentData: {
-      reason: "ð",
-      dueDate: "",
-    },
-    signer: {
-      type: Types.Identity.TYPE.ETHEREUM_ADDRESS,
-      value: payeeIdentity,
-    },
-  };
-
-  const request = await requestClient.createRequest(requestCreateParameters);
-  const requestData = await request.waitForConfirmation();
-
-  res.status(200).send({
-    message: "Request created successfully.",
-    requestID: requestData.requestId,
-  });
+    };
+  
+    const request = await requestClient.createRequest(requestCreateParameters);
+    const requestData = await request.waitForConfirmation();
+  
+    res.status(200).send({
+      message: "Request created successfully.",
+      requestID: requestData.requestId,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error", error });
+  }
 });
 
 app.get("/requests/:userAddress", async (req, res) => {
